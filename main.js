5ff7568c3f0945749cc405d55ce451f81c086470
Jamshidbek <jamshidbekmirzakarimov6@gmail.com>
2023-01-13 19:16:48 +0500

    algorithm finished

--- main.js
+++ main.js
@@ -4,6 +4,7 @@
 
 // console.log(s);
 
+//3-masala
 // let a = prompt("eni");
 // let b = prompt("bo'yi");
 
@@ -15,81 +16,79 @@
 // console.log(PI * d);
 
 
-// let ism = prompt("ismingizni kiriting");
-// let age = prompt("yoshingizni kiriting");
+//4-masala
 
-// if(age >= 18){
-//   alert("kirishingiz mumkin");
-// }else if(age < 18){
-//   alert("kirishingiz mumkin emas");
-// } else {
-//   alert("raqam kiriting");
-// }
-  
-// let number = prompt("son kiriting");
 
-// if (number % 2 == 0) {
-//   alert("juft son");
-// } else if (number % 2 !== 0) {
-//   alert("tog son");
-// } else {
-//   alert("Number kiriting");
-// }
+// const PI = 3.14;
 
-// let number = prompt("son kiriting");
+// let d = prompt("son kiritng");
 
-// if (number > 0) {
-//   alert("musbat son");
-// } else if (number < 0) {
-//   alert("manfiy son");
-// } else if (number == 0) {
-//   alert("manfiy ham emas musbat ham emas");
-// } else {
-//   alert("Number kiriting ");
-// }
+// l = d * PI;
 
-console.log(`
-         *
-       * * * 
-         *
-        ***
-       *****
-      *******
-     *********
-    ***********
-   *************
-  ***************
- *****************
-*******************
-     ****
-     ****
-     ****
-     
+// console.log(l);
 
-     ⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⣶⣶⣷⣶⣶⣿⣿⣿⣷⣾⣽⣒⢤⡀⠀⠀⠀⠀
-⠀⠀⢣⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣽⣆⠀⠀⠀
-⠀⠀⡼⣿⣿⣿⡿⠿⠿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⠀⠀
-⠀⢠⣧⣿⠋⠁⠀⠀⠀⠀⠈⠉⠉⠙⠛⠛⠿⣿⣿⣿⣿⣿⣧⠀
-⠀⢸⣽⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⣿⣿⣿⡟⡇
-⠀⢸⠇⠀⠀⢀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣷⢱
-⠀⢸⠀⠈⣉⣙⣿⡆⠀⠀⣰⣿⣟⢛⣛⣶⣄⠀⠀⢨⣿⣿⣿⡎
-⠀⣼⠀⠘⠉⢉⠙⠁⠀⠀⠛⠛⣙⢛⡛⠻⠿⠄⠀⢸⣿⣿⣿⠇
-⢸⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⣉⣙⡆
-⢸⠀⠀⠀⠀⠀⠤⢠⣶⣦⡤⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⠛⡇
-⡸⠀⠀⠀⠀⢀⣀⡘⣛⡉⠉⠀⠀⠀⠀⠀⠀⠀⠀⢀⠈⠉⢠⡇
-⠹⡄⠀⠀⠰⠿⢋⠉⠋⣛⣿⡷⠆⠀⠀⠀⠀⠀⠀⣬⣀⣠⠞⠀
-⠀⢳⠀⠀⠀⠀⣀⣀⣠⣤⡶⠂⠀⠀⠀⠀⠀⠀⠀⣿⡟⠀⠀⠀
-⠀⠘⣆⠀⠀⠀⠀⠉⠉⠀⠀⠀⠀⠀⢀⣀⣤⣶⣾⠿⡇⠀⠀⠀
-⠀⠀⠈⣦⠀⠀⣀⣀⣀⣤⣤⣴⣶⣿⣿⣿⣿⣿⣿⠀⣇⠀⠀⠀
-⠀⠀⠀⢹⠀⠈⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠁⠠⠿⢤⡀⠀
-⠀⠀⠀⢸⠀⠀⠀⠈⠻⣿⣿⣿⣿⣿⣿⣿⣿⣷⠀⠀⠑⠀⢱⡀
-⠀⠀⢀⡸⠀⠀⠀⠀⠀⢹⣿⣿⣿⣿⣿⣿⣿⣿⡆⠀⠀⡄⣼⡇
-⠀⡴⠋⠀⠀⠀⠀⠀⠀⢸⣿⣿#byFCA⣿⣿⣷⠀⣰⡝⢸⠇
-⠀⠘⡆⠀⠀⠀⠀⠀⢀⣤⣤⣿⣿⣿⣿⣿⣿⡿⠃⣴⡟⢀⠎⠀
-⠀⠀⢹⡄⠀⡀⠀⠀⢸⣿⣿⣿⣿⣿⣿⡿⠋⣠⣾⠋⣰⠋⠀⠀
-⠀⠀⠈⢧⠀⠹⣦⡀⠈⠻⣿⣿⣿⡿⠋⣴⡾⠛⢀⠜⠁⠀⠀⠀
-⠀⠀⠀⠈⢷⡀⠈⢻⣦⡀⠘⠋⢁⣴⡿⠋⢀⡴⠁⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠻⣦⡀⠙⠟⢀⣴⠟⠉⣠⠔⠉⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠈⠳⠦⣴⠿⠷⠒⠊⠁⠀⠀⠀⠀⠀
-`);
+//5-masala
+
+// let a = prompt("son kiriting");
+
+// let b = 6;
+
+// console.log(6 * Math.pow(a, 2));
+
+
+//6-masala
+
+// let a = prompt("a ni o'rniga kiriting");
+// let b = prompt("b ni o'rniga kiriting");
+// let c = prompt("c ni o'rniga kiriting");
+
+// v = a * b * c;
+
+// s = 2 * (a * b + b * c + a * c);
+
+
+// console.log("Hajmi:",v, "Sirti:",s);
+
+
+// 7-masala
+
+// const PI = 3.14;
+// let r = prompt("r ni kiriting");
+
+// l = 2 * PI * r;
+
+// s = PI * Math.pow(r, 2);
+
+// console.log("Uzunligi",l , "yuzasi",s);
+
+// 8-masala
+
+// let a = prompt("1-sonni kiriting");
+
+// let b = prompt("2-sonni kiriting");
+
+// console.log((a + b) / 2);
+
+
+//9-masala
+// let a = prompt("1-sonni kiriting");
+
+// let b = prompt("2-sonni kiriting");
+
+// root = Math.sqrt(a * b);
+
+// console.log(root);
+
+//10-masala
+
+let a = Number(prompt("1-sonni kiriting"));
+let b = Number(prompt("2-sonni kiriting"));
+
+yigindi = a + b;
+
+kopaytma = a * b;
+
+root = Math.pow(a, 2);
+root2 = Math.pow(b, 2);
+console.log("Yig'indi",yigindi, "Kopaytmasi",kopaytma, "Ildiz",root,root2);
+
